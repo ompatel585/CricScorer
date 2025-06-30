877f102eb3c315cf7fc609a97b78acfd91a719c4
Om Patel <ompatel34003@gmail.com>
2025-06-30 16:36:33 +0530

    fixed axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,17 +1,10 @@
-// import axios from "axios";
-
-// const BASE_URL = import.meta.env.MODE === "development"
-//     ? "http://localhost:5001/api"
-//     : "/api";
-
-// const api = axios.create({ baseURL: BASE_URL });
-
-// export default api;
-
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api"
+// in production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
-const api = axios.create({ baseURL: BASE_URL });
+const api = axios.create({
+    baseURL: BASE_URL,
+});
 
 export default api;
